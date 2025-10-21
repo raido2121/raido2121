47576d9192ca8cea4c084efbb0932e32308e7cd1
raido2121 <mkcorpsatu@gmail.com>
2025-10-21 13:34:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementById('sapaSaya');
+const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function() {
 alert('yoooooooo temeeeeee!');
 });
